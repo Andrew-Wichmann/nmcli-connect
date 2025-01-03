1b12100bd99ccc181bfa36d42bb3fe7efa13f990
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-01-03 16:51:28 -0500

    feat: add dummy connecting logic

--- cmd/nmcli-connect/connector.go
+++ cmd/nmcli-connect/connector.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -13,16 +14,45 @@ func newConnector(ssid, password string) connector {
 type connector struct {
 	ssid     string
 	password string
+	error    string
+	message  string
 }
 
 func (c connector) Init() tea.Cmd {
-	return nil
+	return c.connect
 }
 
 func (c connector) Update(msg tea.Msg) (connector, tea.Cmd) {
+	if msg, ok := msg.(connectFailed); ok {
+		c.error = msg.err.Error()
+	}
+	if _, ok := msg.(connectSucceeded); ok {
+		c.message = "Connected!"
+	}
 	return c, nil
 }
 
 func (c connector) View() string {
+	if c.error != "" {
+		return c.error
+	}
+	if c.message != "" {
+		return c.message
+	}
 	return fmt.Sprintf("beep boop. connecting to %s with password: %s", c.ssid, c.password)
+}
+
+type connectFailed struct {
+	err error
+}
+
+type connectSucceeded struct{}
+
+func (c connector) connect() tea.Msg {
+	cmd := exec.Command("sleep", "10")
+	_, err := cmd.Output()
+	if err != nil {
+		return connectFailed{err: err}
+	}
+	return connectSucceeded{}
 }
